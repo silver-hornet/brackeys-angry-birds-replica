f1a924cf9b3ce0cf434902b629c37441c3e5071e
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-09 11:22:51 -0500

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float releaseTime = 0.15f;
+    bool isPressed;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 4f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
